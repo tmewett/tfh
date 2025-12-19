1837aa283e656bd25e14d3a9778760c0ee54458c
Tom M <tom@tmewett.com>
2025-12-19 17:10:01 +0000

    stuff

--- main.tcl
+++ main.tcl
@@ -5,27 +5,45 @@ oo::class create app {
     variable viewingDir
     constructor {} {
         grid [ttk::button .up -text "Up" -command "[self] upClicked"]
-        grid [ttk::frame .files]
+        grid [ttk::frame .files -width 640 -height 480]
+        grid [ttk::scrollbar .filesScroll -command [list .files yview]] -column 1
+        grid propagate .files false
         set iDir [pwd]
         set viewingDir ""
-    }
-    method itemClicked {path} {
-        puts $path
     }
     method upClicked {} {
         set iDir [file dirname $iDir]
         my update
     }
+    method fileClicked {path} {
+        set iDir $iDir/$path
+        my update
+    }
     method update {} {
         if {$iDir != $viewingDir} {
             cd $iDir
-            set files [glob *]
+            set entries [glob *]
             foreach child [winfo children .files] {
                 destroy $child
             }
+            set dirs [list]
+            set files [list]
+            foreach entry $entries {
+                if [file isdirectory $entry] {
+                    lappend dirs $entry
+                } else {
+                    lappend files $entry
+                }
+            }
             set i 0
-            foreach file $files {
-                grid [ttk::label .files.$i -text $file]
+            foreach entry $dirs {
+                grid [ttk::label .files.$i -text $entry/ -foreground "blue violet"] -sticky w
+                bind .files.$i <1> [list [self] fileClicked $entry]
+                incr i
+            }
+            foreach entry $files {
+                grid [ttk::label .files.$i -text $entry] -sticky w
+                bind .files.$i <1> [list [self] fileClicked $entry]
                 incr i
             }
             set $viewingDir $iDir
